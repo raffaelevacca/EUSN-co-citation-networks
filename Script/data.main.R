b692a3fdf2cf856ddf9dcf43b4512dcb7091658e
raffaele.vacca <r.vacca@ufl.edu>
2014-05-26 13:39:26 -0400

    Added data from 1996-2007
    Added row data from 1996-2007. Edited data.main.R to import them.

--- Script/data.main.R
+++ Script/data.main.R
@@ -53,9 +53,68 @@ source("./Script/data.01.R")
 social.10 <- data[discipline$social,]
 comp_physics.10 <- data[discipline$comp_physics,]
 
+## 2005-2007
+## =================================================================================================
+
+# Get names of all text files with Web of Science data for relevant timespan
+files <- list.files("./Data", pattern= "2007.+txt")
+
+# Source data management script
+source("./Script/data.01.R")
+
+# table(discipline[,1:2])
+
+# Split the data
+social.07 <- data[discipline$social,]
+comp_physics.07 <- data[discipline$comp_physics,]
+
+
+## 2002-2004
+## =================================================================================================
+
+# Get names of all text files with Web of Science data for relevant timespan
+files <- list.files("./Data", pattern= "2004.+txt")
+
+# Source data management script
+source("./Script/data.01.R")
+
+# table(discipline[,1:2])
+
+# Split the data
+social.04 <- data[discipline$social,]
+comp_physics.04 <- data[discipline$comp_physics,]
+
+## 1999-2001
+## =================================================================================================
+
+# Get names of all text files with Web of Science data for relevant timespan
+files <- list.files("./Data", pattern= "2001.+txt")
+
+# Source data management script
+source("./Script/data.01.R")
+
+# table(discipline[,1:2])
+
+# Split the data
+social.01 <- data[discipline$social,]
+comp_physics.01 <- data[discipline$comp_physics,]
+
+## 1996-1998
+## =================================================================================================
+
+# Get names of all text files with Web of Science data for relevant timespan
+files <- list.files("./Data", pattern= "1998.+txt")
+
+# Source data management script
+source("./Script/data.01.R")
+
+# table(discipline[,1:2])
+
+# Split the data
+social.98 <- data[discipline$social,]
+comp_physics.98 <- data[discipline$comp_physics,]
 
 # Save all
 save(list= ls(pattern="social\\.[0-9]|comp_physics\\.[0-9]"), file="./Data/records.rda")
-
 
 
